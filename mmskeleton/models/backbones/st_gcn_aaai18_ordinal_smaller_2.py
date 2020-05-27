c221d2b0c49bdab408593d69856ef718b37a1408
Andrea Sabo <andrea.sabo@mail.utoronto.ca>
2020-05-26 21:14:20 -0400

    fix indent

--- mmskeleton/models/backbones/st_gcn_aaai18_ordinal_smaller_2.py
+++ mmskeleton/models/backbones/st_gcn_aaai18_ordinal_smaller_2.py
@@ -48,13 +48,13 @@ class ST_GCN_18_ordinal_smaller_2(nn.Module):
         self.data_bn = nn.BatchNorm1d(
             in_channels * A.size(1)) if data_bn else lambda x: x
         kwargs0 = {k: v for k, v in kwargs.items() if k != 'dropout'}
-      self.st_gcn_networks = nn.ModuleList((
-            st_gcn_block(
-                in_channels, 32, kernel_size, 1, residual=False, **kwargs0),
-            st_gcn_block(32, 32, kernel_size, 1, **kwargs),
-            st_gcn_block(32, 64, kernel_size, 2, **kwargs),
-            st_gcn_block(64, 64, kernel_size, 1, **kwargs),
-        ))
+        self.st_gcn_networks = nn.ModuleList((
+                st_gcn_block(
+                    in_channels, 32, kernel_size, 1, residual=False, **kwargs0),
+                st_gcn_block(32, 32, kernel_size, 1, **kwargs),
+                st_gcn_block(32, 64, kernel_size, 2, **kwargs),
+                st_gcn_block(64, 64, kernel_size, 1, **kwargs),
+            ))
 
 
         # initialize parameters for edge importance weighting
