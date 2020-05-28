d70a349647ae62764f3097afcdbbb58471213b9b
Andrea Sabo <andrea.sabo@mail.utoronto.ca>
2020-05-28 19:37:35 -0400

    zero_shift

---
+++ batch/UPDRS_2_shift_0.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1              # Number of GPUs (per node)
+#SBATCH --mem=8000               # memory (per node)
+#SBATCH --cpus-per-task=8
+#SBATCH --time=0-22:00            # time (DD-HH:MM)
+#SBATCH --job-name=zs_simpl2_updrs_150
+#SBATCH --output=%x-%j_32hour.out
+#SBATCH --account=def-btaati
+
+#SBATCH --mail-user=andrea.sabo@mail.utoronto.ca
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
+
+
+module load python/3.6
+module load nixpkgs/16.09  gcc/8.3.0 
+module load cuda/10.1
+
+source ~/ENV/bin/activate
+
+cd /home/asabo/projects/def-btaati/asabo/mmskeleton
+python mmskl.py configs/recognition/tri/cluster/train_cluster_UPDRS_2_shift_0.yaml
