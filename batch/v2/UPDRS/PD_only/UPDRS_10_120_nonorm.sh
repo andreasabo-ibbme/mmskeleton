1424956925515637a0ce159b03381776da7eb326
Andrea Sabo <51453348+andreasabo-ibbme@users.noreply.github.com>
2020-06-27 12:05:47 -0400

    pd only fix

--- batch/v2/UPDRS/PD_only/UPDRS_10_120_nonorm.sh
+++ batch/v2/UPDRS/PD_only/UPDRS_10_120_nonorm.sh
@@ -3,12 +3,16 @@
 #SBATCH --mem=8000               # memory (per node)
 #SBATCH --cpus-per-task=8
 #SBATCH --time=0-04:00            # time (DD-HH:MM)
-#SBATCH --job-name=v2_simpl2_sas_150
+#SBATCH --job-name=v2_nn_simpl10_updrs_120
 #SBATCH --output=%x-%j_32hour.out
 #SBATCH --account=def-btaati
 
 #SBATCH --mail-user=andrea.sabo@mail.utoronto.ca
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
 
 
 module load python/3.6
@@ -18,4 +22,4 @@ module load cuda/10.1
 source ~/ENV/bin/activate
 
 cd /home/asabo/projects/def-btaati/asabo/mmskeleton
-python mmskl.py configs/recognition/tri/cluster/v2/SAS/PD_only/train_cluster_SAS_2.yaml
+python mmskl.py configs/recognition/tri/cluster/v2/UPDRS/PD_only/train_cluster_UPDRS_10_120_nonorm.yaml
