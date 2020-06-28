1b3ecbca771a0fe8550816a78a48630f7726c759
Andrea Sabo <51453348+andreasabo-ibbme@users.noreply.github.com>
2020-06-28 19:47:30 -0400

    fix start all

--- batch/v2/start_all.sh
+++ batch/v2/start_all.sh
@@ -14,14 +14,14 @@ bash start_UPDRS_10.sh
 
 cd /home/asabo/projects/def-btaati/asabo/mmskeleton/batch/v2
 cd ./SAS/
-bash start_UPDRS_2.sh
-bash start_UPDRS_10.sh
+bash start_SAS_2.sh
+bash start_SAS_10.sh
 
 cd ./PD_only/
-bash start_UPDRS_2.sh
-bash start_UPDRS_10.sh
+bash start_SAS_2.sh
+bash start_SAS_10.sh
 
 cd ./lrd40/
-bash start_UPDRS_2.sh
-bash start_UPDRS_10.sh
+bash start_SAS_2.sh
+bash start_SAS_10.sh
 
