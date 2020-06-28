ec9b50eaa5e78489eaa8eeab53a6c808bdd0513b
Andrea Sabo <51453348+andreasabo-ibbme@users.noreply.github.com>
2020-06-28 19:31:24 -0400

    min 100 epochs of training

---
+++ batch/v2/start_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+bash UPDRS/start_UPDRS_2.sh
+bash UPDRS/start_UPDRS_10.sh
+bash UPDRS/PD_only/start_UPDRS_2.sh
+bash UPDRS/PD_only/start_UPDRS_10.sh
+bash UPDRS/PD_only/lrd40/start_UPDRS_2.sh
+bash UPDRS/PD_only/lrd40/start_UPDRS_10.sh
+
+bash SAS/start_SAS_2.sh
+bash SAS/start_SAS_10.sh
+bash SAS/PD_only/start_SAS_2.sh
+bash SAS/PD_only/start_SAS_10.sh
+bash SAS/PD_only/lrd40/start_SAS_2.sh
+bash SAS/PD_only/lrd40/start_SAS_10.sh
+
+
