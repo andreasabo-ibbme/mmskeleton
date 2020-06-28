4486ac671988851b9f1487091ac8820360c7ba7c
Andrea Sabo <51453348+andreasabo-ibbme@users.noreply.github.com>
2020-06-28 19:42:38 -0400

    fix start all

--- batch/v2/UPDRS/start_UPDRS_2.sh
+++ batch/v2/UPDRS/start_UPDRS_2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sbatch UPDRS_2_120_nonorm.sh
-sbatch UPDRS_2_120.sh
-sbatch UPDRS_2_nonorm.sh
-sbatch UPDRS_2.sh
+sbatch ./UPDRS_2_120_nonorm.sh
+sbatch ./UPDRS_2_120.sh
+sbatch ./UPDRS_2_nonorm.sh
+sbatch ./UPDRS_2.sh
