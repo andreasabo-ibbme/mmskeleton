0abd7ff1196f10c203f6ada15f5718d7d1bfd3e8
Andrea Sabo <51453348+andreasabo-ibbme@users.noreply.github.com>
2020-06-29 09:05:25 -0400

    split start

--- batch/v2/start_all_SAS.sh
+++ batch/v2/start_all_SAS.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-cd ./UPDRS/
-bash start_UPDRS_2.sh
-bash start_UPDRS_10.sh
-
-cd ./PD_only/
-bash start_UPDRS_2.sh
-bash start_UPDRS_10.sh
-
-cd ./lrd40/
-bash start_UPDRS_2.sh
-bash start_UPDRS_10.sh
-
 cd /home/asabo/projects/def-btaati/asabo/mmskeleton/batch/v2
 cd ./SAS/
 bash start_SAS_2.sh
---
+++ batch/v2/start_all_UPDRS.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ./UPDRS/
+bash start_UPDRS_2.sh
+bash start_UPDRS_10.sh
+
+cd ./PD_only/
+bash start_UPDRS_2.sh
+bash start_UPDRS_10.sh
+
+cd ./lrd40/
+bash start_UPDRS_2.sh
+bash start_UPDRS_10.sh
