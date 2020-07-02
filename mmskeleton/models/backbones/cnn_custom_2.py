4eff620da2c15acec3ee274031584ac4bebc4060
Andrea Sabo <asabo@edu.uwaterloo.ca>
2020-07-02 12:42:13 -0400

    larger temporal

--- mmskeleton/models/backbones/cnn_custom_2.py
+++ mmskeleton/models/backbones/cnn_custom_2.py
@@ -46,10 +46,10 @@ class cnn_custom_2(nn.Module):
         self.data_bn = nn.BatchNorm3d(1) if data_bn else lambda x: x
 
 
-        self.temporal_kernel = 9
-        self.conv1_filters = 32
-        self.conv2_filters = 64
-        self.conv3_filters = 64
+        self.temporal_kernel = 15
+        self.conv1_filters = 16
+        self.conv2_filters = 32
+        self.conv3_filters = 32
         self.fc1_out = 128
 
         # build the CNN
