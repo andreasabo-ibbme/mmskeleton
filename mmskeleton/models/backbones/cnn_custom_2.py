5ca256f2fb2b79363c918af5523c065cdc20f1df
Andrea Sabo <asabo@edu.uwaterloo.ca>
2020-07-02 12:38:19 -0400

    bigger model

--- mmskeleton/models/backbones/cnn_custom_2.py
+++ mmskeleton/models/backbones/cnn_custom_2.py
@@ -47,9 +47,9 @@ class cnn_custom_2(nn.Module):
 
 
         self.temporal_kernel = 9
-        self.conv1_filters = 16
-        self.conv2_filters = 32
-        self.conv3_filters = 32
+        self.conv1_filters = 32
+        self.conv2_filters = 64
+        self.conv3_filters = 64
         self.fc1_out = 128
 
         # build the CNN
