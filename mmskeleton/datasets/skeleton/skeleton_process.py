9d34a43404b30770e3f5393534925b2d525c1dbe
Andrea Sabo <asabo@edu.uwaterloo.ca>
2020-07-02 18:53:28 -0400

    log more

--- mmskeleton/datasets/skeleton/skeleton_process.py
+++ mmskeleton/datasets/skeleton/skeleton_process.py
@@ -171,6 +171,7 @@ def pad_mean(data, size):
 
         np_array = data[data_field]
         mean_val = np.mean(np_array)
+        print("np array is: ", np_array)
         print("mean val is:", mean_val)
         T = np_array.shape[2]
         if T < size:
