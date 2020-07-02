1c76a3aca68c912be6b5cde8a11e4aabcfd908ab
Andrea Sabo <asabo@edu.uwaterloo.ca>
2020-07-02 14:05:14 -0400

    more logging

--- mmskeleton/processor/recognition_tri.py
+++ mmskeleton/processor/recognition_tri.py
@@ -453,7 +453,7 @@ def batch_processor(model, datas, train_mode, loss):
 
     # Get predictions from the model
     output_all = model(data_all)
-    print(output_all)    
+    print('data all', data_all, "output all: ", output_all)    
     output = output_all[row_cond]
     loss_flip_tensor = torch.tensor([0.], dtype=torch.float, requires_grad=True) 
 
