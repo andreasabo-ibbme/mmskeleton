4fc00f77516cd9bc83c29480a09b0e46864a6ca3
Andrea Sabo <asabo@edu.uwaterloo.ca>
2020-07-03 13:35:51 -0400

    supcon dev

--- mmskeleton/processor/recognition_tri_supcon.py
+++ mmskeleton/processor/recognition_tri_supcon.py
@@ -16,6 +16,7 @@ import spacecutter
 import pandas as pd
 import pickle
 from mmskeleton.processor.utils_recognition import *
+from mmskeleton.processor.supcon_loss import *
 
 os.environ['WANDB_MODE'] = 'dryrun'
 
