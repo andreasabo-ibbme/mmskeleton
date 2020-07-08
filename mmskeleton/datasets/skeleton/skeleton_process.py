ce29c8e93c5e85f8a76c9209e5378c699db6a7df
Andrea Sabo <51453348+andreasabo-ibbme@users.noreply.github.com>
2020-07-08 15:22:42 -0400

    wip

--- mmskeleton/datasets/skeleton/skeleton_process.py
+++ mmskeleton/datasets/skeleton/skeleton_process.py
@@ -269,12 +269,13 @@ def pad_zero_beginning_for_joint_prediction(data, size, pred_ts):
             # Add the future timesteps for prediction to the categrory_id
             # print('data shape: ', np_array_paded.shape)
             output_target = np.zeros([2, pad_shape[1], len(pred_ts)], dtype=np_array.dtype)
-            print("outptu target shape: ", output_target.shape)
+            print("output target shape: ", output_target.shape)
 
             for i in range(len(pred_ts)):
                 t_ind = max_future_ts - pred_ts[i] + 1
                 joint_data = all_data[0:2, :, -t_ind]
                 joint_data = joint_data.squeeze()
+                print("joint_data: ", joint_data.shape)
                 output_target[:, :, pred_ts] = joint_data
                 data['category_id'] = output_target
 
