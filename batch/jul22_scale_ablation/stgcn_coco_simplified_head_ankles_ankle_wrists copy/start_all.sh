699773f6af884573fe549317e6008bb75ddf3ed3
Andrea Sabo <andrea.sabo@mail.utoronto.ca>
2020-07-22 13:47:32 -0400

    add bash files for jul22 ablation

---
+++ batch/jul22_scale_ablation/stgcn_coco_simplified_head_ankles_ankle_wrists copy/start_all.sh
@@ -0,0 +1,143 @@
+cd /home/asabo/projects/def-btaati/asabo/mmskeleton/batch/jul22_scale_ablation/pretrain/SAS/v3
+
+# SAS pretrain
+sbatch SAS_0_120_pred_15.sh
+sbatch SAS_0_120_pred_15_do_0.1.sh
+sbatch SAS_0_120_pred_15_ft_all.sh
+sbatch SAS_0_120_pred_15_do_0.1_ft_all.sh
+
+sbatch SAS_2_120_pred_15.sh
+sbatch SAS_2_120_pred_15_do_0.1.sh
+sbatch SAS_2_120_pred_15_ft_all.sh
+sbatch SAS_2_120_pred_15_do_0.1_ft_all.sh
+
+sbatch SAS_10_120_pred_15.sh
+sbatch SAS_10_120_pred_15_do_0.1.sh
+sbatch SAS_10_120_pred_15_ft_all.sh
+sbatch SAS_10_120_pred_15_do_0.1_ft_all.sh
+
+# UPDRS pretrain
+cd /home/asabo/projects/def-btaati/asabo/mmskeleton/batch/jul22_scale_ablation/pretrain/UPDRS/v3
+sbatch SAS_0_120_pred_15.sh
+sbatch SAS_0_120_pred_15_do_0.1.sh
+sbatch SAS_0_120_pred_15_ft_all.sh
+sbatch SAS_0_120_pred_15_do_0.1_ft_all.sh
+
+sbatch SAS_2_120_pred_15.sh
+sbatch SAS_2_120_pred_15_do_0.1.sh
+sbatch SAS_2_120_pred_15_ft_all.sh
+sbatch SAS_2_120_pred_15_do_0.1_ft_all.sh
+
+sbatch SAS_10_120_pred_15.sh
+sbatch SAS_10_120_pred_15_do_0.1.sh
+sbatch SAS_10_120_pred_15_ft_all.sh
+sbatch SAS_10_120_pred_15_do_0.1_ft_all.sh
+
+
+cd /home/asabo/projects/def-btaati/asabo/mmskeleton/batch/jul22_scale_ablation/pretrain_coco_simplified_head_ankles_ankle_wrists/SAS/v3
+
+# SAS pretrain_coco_simplified_head_ankles_ankle_wrists
+sbatch SAS_0_120_pred_15.sh
+sbatch SAS_0_120_pred_15_do_0.1.sh
+sbatch SAS_0_120_pred_15_ft_all.sh
+sbatch SAS_0_120_pred_15_do_0.1_ft_all.sh
+
+sbatch SAS_2_120_pred_15.sh
+sbatch SAS_2_120_pred_15_do_0.1.sh
+sbatch SAS_2_120_pred_15_ft_all.sh
+sbatch SAS_2_120_pred_15_do_0.1_ft_all.sh
+
+sbatch SAS_10_120_pred_15.sh
+sbatch SAS_10_120_pred_15_do_0.1.sh
+sbatch SAS_10_120_pred_15_ft_all.sh
+sbatch SAS_10_120_pred_15_do_0.1_ft_all.sh
+
+# UPDRS pretrain_coco_simplified_head_ankles_ankle_wrists
+cd /home/asabo/projects/def-btaati/asabo/mmskeleton/batch/jul22_scale_ablation/pretrain_coco_simplified_head_ankles_ankle_wrists/UPDRS/v3
+sbatch SAS_0_120_pred_15.sh
+sbatch SAS_0_120_pred_15_do_0.1.sh
+sbatch SAS_0_120_pred_15_ft_all.sh
+sbatch SAS_0_120_pred_15_do_0.1_ft_all.sh
+
+sbatch SAS_2_120_pred_15.sh
+sbatch SAS_2_120_pred_15_do_0.1.sh
+sbatch SAS_2_120_pred_15_ft_all.sh
+sbatch SAS_2_120_pred_15_do_0.1_ft_all.sh
+
+sbatch SAS_10_120_pred_15.sh
+sbatch SAS_10_120_pred_15_do_0.1.sh
+sbatch SAS_10_120_pred_15_ft_all.sh
+sbatch SAS_10_120_pred_15_do_0.1_ft_all.sh
+
+## STGCN
+
+cd /home/asabo/projects/def-btaati/asabo/mmskeleton/batch/jul22_scale_ablation/stgcn/SAS/v3
+
+# SAS pretrain
+sbatch SAS_0_120_pred_15.sh
+sbatch SAS_0_120_pred_15_do_0.1.sh
+sbatch SAS_0_120_pred_15_ft_all.sh
+sbatch SAS_0_120_pred_15_do_0.1_ft_all.sh
+
+sbatch SAS_2_120_pred_15.sh
+sbatch SAS_2_120_pred_15_do_0.1.sh
+sbatch SAS_2_120_pred_15_ft_all.sh
+sbatch SAS_2_120_pred_15_do_0.1_ft_all.sh
+
+sbatch SAS_10_120_pred_15.sh
+sbatch SAS_10_120_pred_15_do_0.1.sh
+sbatch SAS_10_120_pred_15_ft_all.sh
+sbatch SAS_10_120_pred_15_do_0.1_ft_all.sh
+
+# UPDRS pretrain
+cd /home/asabo/projects/def-btaati/asabo/mmskeleton/batch/jul22_scale_ablation/stgcn/UPDRS/v3
+sbatch SAS_0_120_pred_15.sh
+sbatch SAS_0_120_pred_15_do_0.1.sh
+sbatch SAS_0_120_pred_15_ft_all.sh
+sbatch SAS_0_120_pred_15_do_0.1_ft_all.sh
+
+sbatch SAS_2_120_pred_15.sh
+sbatch SAS_2_120_pred_15_do_0.1.sh
+sbatch SAS_2_120_pred_15_ft_all.sh
+sbatch SAS_2_120_pred_15_do_0.1_ft_all.sh
+
+sbatch SAS_10_120_pred_15.sh
+sbatch SAS_10_120_pred_15_do_0.1.sh
+sbatch SAS_10_120_pred_15_ft_all.sh
+sbatch SAS_10_120_pred_15_do_0.1_ft_all.sh
+
+
+cd /home/asabo/projects/def-btaati/asabo/mmskeleton/batch/jul22_scale_ablation/stgcn_coco_simplified_head_ankles_ankle_wrists/SAS/v3
+
+# SAS pretrain_coco_simplified_head_ankles_ankle_wrists
+sbatch SAS_0_120_pred_15.sh
+sbatch SAS_0_120_pred_15_do_0.1.sh
+sbatch SAS_0_120_pred_15_ft_all.sh
+sbatch SAS_0_120_pred_15_do_0.1_ft_all.sh
+
+sbatch SAS_2_120_pred_15.sh
+sbatch SAS_2_120_pred_15_do_0.1.sh
+sbatch SAS_2_120_pred_15_ft_all.sh
+sbatch SAS_2_120_pred_15_do_0.1_ft_all.sh
+
+sbatch SAS_10_120_pred_15.sh
+sbatch SAS_10_120_pred_15_do_0.1.sh
+sbatch SAS_10_120_pred_15_ft_all.sh
+sbatch SAS_10_120_pred_15_do_0.1_ft_all.sh
+
+# UPDRS pretrain_coco_simplified_head_ankles_ankle_wrists
+cd /home/asabo/projects/def-btaati/asabo/mmskeleton/batch/jul22_scale_ablation/stgcn_coco_simplified_head_ankles_ankle_wrists/UPDRS/v3
+sbatch SAS_0_120_pred_15.sh
+sbatch SAS_0_120_pred_15_do_0.1.sh
+sbatch SAS_0_120_pred_15_ft_all.sh
+sbatch SAS_0_120_pred_15_do_0.1_ft_all.sh
+
+sbatch SAS_2_120_pred_15.sh
+sbatch SAS_2_120_pred_15_do_0.1.sh
+sbatch SAS_2_120_pred_15_ft_all.sh
+sbatch SAS_2_120_pred_15_do_0.1_ft_all.sh
+
+sbatch SAS_10_120_pred_15.sh
+sbatch SAS_10_120_pred_15_do_0.1.sh
+sbatch SAS_10_120_pred_15_ft_all.sh
+sbatch SAS_10_120_pred_15_do_0.1_ft_all.sh
