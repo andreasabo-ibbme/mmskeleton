485ae6db5c7e65c7ac0fb08c2a2da1adf578e2db
Andrea Sabo <51453348+andreasabo-ibbme@users.noreply.github.com>
2020-07-26 11:53:00 -0400

    fix baseline batch files

--- batch/jul22_scale_ablation/stgcn_coco_simplified_head_ankles_ankle_wrists/UPDRS/v3/SAS_10_120_pred_15_do_0.1_ft_all.sh
+++ batch/jul22_scale_ablation/stgcn_coco_simplified_head_ankles_ankle_wrists/UPDRS/v3/SAS_10_120_pred_15_do_0.1_ft_all.sh
@@ -18,4 +18,4 @@ module load cuda/10.1
 source ~/ENV/bin/activate
 
 cd /home/asabo/projects/def-btaati/asabo/mmskeleton
-python mmskl.py configs/recognition/tri/cluster/stgcn_coco_simplified_head_ankles_ankle_wrists/SAS/v3/train_cluster_10_120_pred_15_4_joints_do_0.1_ft_all.yaml
+python mmskl.py configs/recognition/tri/cluster/stgcn_coco_simplified_head_ankles_ankle_wrists/UPDRS/v3/train_cluster_SAS_10_120_nonorm_do_0.1_ft_all.yaml
--- batch/jul22_scale_ablation/stgcn_coco_simplified_head_ankles_ankle_wrists/UPDRS/v3/SAS_2_120_pred_15_do_0.1_ft_all.sh
+++ batch/jul22_scale_ablation/stgcn_coco_simplified_head_ankles_ankle_wrists/UPDRS/v3/SAS_2_120_pred_15_do_0.1_ft_all.sh
@@ -18,4 +18,4 @@ module load cuda/10.1
 source ~/ENV/bin/activate
 
 cd /home/asabo/projects/def-btaati/asabo/mmskeleton
-python mmskl.py configs/recognition/tri/cluster/stgcn_coco_simplified_head_ankles_ankle_wrists/SAS/v3/train_cluster_2_120_pred_15_4_joints_do_0.1.yaml
+python mmskl.py configs/recognition/tri/cluster/stgcn_coco_simplified_head_ankles_ankle_wrists/UPDRS/v3/train_cluster_SAS_2_120_nonorm_do_0.1_ft_all.yaml
