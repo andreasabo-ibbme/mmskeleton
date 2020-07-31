a4c37365d18230f0b81046d1b58120a880b91876
Andrea Sabo <andrea.sabo@mail.utoronto.ca>
2020-07-31 13:20:05 -0400

    startup scripts

---
+++ batch/jul22_scale_ablation/start_all_pretrain_jul31_SAS.sh
@@ -0,0 +1,87 @@
+cd /home/asabo/projects/def-btaati/asabo/mmskeleton/batch/jul22_scale_ablation/pretrain/SAS/v4/temp5
+
+# SAS pretrain
+sbatch SAS_0_120_pred_15.sh
+sbatch SAS_0_120_pred_15_do_0.1.sh
+sbatch SAS_0_120_pred_15_do_0.2.sh
+
+sbatch SAS_2_120_pred_15.sh
+sbatch SAS_2_120_pred_15_do_0.1.sh
+sbatch SAS_2_120_pred_15_do_0.2.sh
+
+sbatch SAS_10_120_pred_15.sh
+sbatch SAS_10_120_pred_15_do_0.1.sh
+sbatch SAS_10_120_pred_15_do_0.2.sh
+
+cd /home/asabo/projects/def-btaati/asabo/mmskeleton/batch/jul22_scale_ablation/pretrain/SAS/v4/temp9
+
+# SAS pretrain
+sbatch SAS_0_120_pred_15.sh
+sbatch SAS_0_120_pred_15_do_0.1.sh
+sbatch SAS_0_120_pred_15_do_0.2.sh
+
+sbatch SAS_2_120_pred_15.sh
+sbatch SAS_2_120_pred_15_do_0.1.sh
+sbatch SAS_2_120_pred_15_do_0.2.sh
+
+sbatch SAS_10_120_pred_15.sh
+sbatch SAS_10_120_pred_15_do_0.1.sh
+sbatch SAS_10_120_pred_15_do_0.2.sh
+
+cd /home/asabo/projects/def-btaati/asabo/mmskeleton/batch/jul22_scale_ablation/pretrain/SAS/v4/temp13
+
+# SAS pretrain
+sbatch SAS_0_120_pred_15.sh
+sbatch SAS_0_120_pred_15_do_0.1.sh
+sbatch SAS_0_120_pred_15_do_0.2.sh
+
+sbatch SAS_2_120_pred_15.sh
+sbatch SAS_2_120_pred_15_do_0.1.sh
+sbatch SAS_2_120_pred_15_do_0.2.sh
+
+sbatch SAS_10_120_pred_15.sh
+sbatch SAS_10_120_pred_15_do_0.1.sh
+sbatch SAS_10_120_pred_15_do_0.2.sh
+
+
+cd /home/asabo/projects/def-btaati/asabo/mmskeleton/batch/jul22_scale_ablation/pretrain_coco_simplified_head_ankles_ankle_wrists/SAS/v4/temp5
+# SAS pretrain
+sbatch SAS_0_120_pred_15.sh
+sbatch SAS_0_120_pred_15_do_0.1.sh
+sbatch SAS_0_120_pred_15_do_0.2.sh
+
+sbatch SAS_2_120_pred_15.sh
+sbatch SAS_2_120_pred_15_do_0.1.sh
+sbatch SAS_2_120_pred_15_do_0.2.sh
+
+sbatch SAS_10_120_pred_15.sh
+sbatch SAS_10_120_pred_15_do_0.1.sh
+sbatch SAS_10_120_pred_15_do_0.2.sh
+
+cd /home/asabo/projects/def-btaati/asabo/mmskeleton/batch/jul22_scale_ablation/pretrain_coco_simplified_head_ankles_ankle_wrists/SAS/v4/temp9
+# SAS pretrain
+sbatch SAS_0_120_pred_15.sh
+sbatch SAS_0_120_pred_15_do_0.1.sh
+sbatch SAS_0_120_pred_15_do_0.2.sh
+
+sbatch SAS_2_120_pred_15.sh
+sbatch SAS_2_120_pred_15_do_0.1.sh
+sbatch SAS_2_120_pred_15_do_0.2.sh
+
+sbatch SAS_10_120_pred_15.sh
+sbatch SAS_10_120_pred_15_do_0.1.sh
+sbatch SAS_10_120_pred_15_do_0.2.sh
+
+cd /home/asabo/projects/def-btaati/asabo/mmskeleton/batch/jul22_scale_ablation/pretrain_coco_simplified_head_ankles_ankle_wrists/SAS/v4/temp13
+# SAS pretrain
+sbatch SAS_0_120_pred_15.sh
+sbatch SAS_0_120_pred_15_do_0.1.sh
+sbatch SAS_0_120_pred_15_do_0.2.sh
+
+sbatch SAS_2_120_pred_15.sh
+sbatch SAS_2_120_pred_15_do_0.1.sh
+sbatch SAS_2_120_pred_15_do_0.2.sh
+
+sbatch SAS_10_120_pred_15.sh
+sbatch SAS_10_120_pred_15_do_0.1.sh
+sbatch SAS_10_120_pred_15_do_0.2.sh
