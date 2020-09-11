053c8aa7df95e1d6b0f0072e154da8dd6e6ac694
Andrea Sabo <andrea.sabo@mail.utoronto.ca>
2020-09-11 18:16:38 -0400

    separate SAS and UPDRS for start scripts

---
+++ batch/self_train/start_all_v1_SAS.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# SAS
+cd /home/asabo/projects/def-btaati/asabo/mmskeleton/batch/self_train/SAS/v1/temp5
+sbatch SAS_0_120_pred_15_do_0.0.sh
+sbatch SAS_0_120_pred_15_do_0.1.sh
+sbatch SAS_0_120_pred_15_do_0.2.sh
+sbatch SAS_0_120_pred_15_do_0.3.sh
+sbatch SAS_0_120_pred_15_do_0.4.sh
+sbatch SAS_0_120_pred_15_do_0.5.sh
+
+cd /home/asabo/projects/def-btaati/asabo/mmskeleton/batch/self_train/SAS/v1/temp9
+sbatch SAS_0_120_pred_15_do_0.0.sh
+sbatch SAS_0_120_pred_15_do_0.1.sh
+sbatch SAS_0_120_pred_15_do_0.2.sh
+sbatch SAS_0_120_pred_15_do_0.3.sh
+sbatch SAS_0_120_pred_15_do_0.4.sh
+sbatch SAS_0_120_pred_15_do_0.5.sh
+
+cd /home/asabo/projects/def-btaati/asabo/mmskeleton/batch/self_train/SAS/v1/temp13
+sbatch SAS_0_120_pred_15_do_0.0.sh
+sbatch SAS_0_120_pred_15_do_0.1.sh
+sbatch SAS_0_120_pred_15_do_0.2.sh
+sbatch SAS_0_120_pred_15_do_0.3.sh
+sbatch SAS_0_120_pred_15_do_0.4.sh
+sbatch SAS_0_120_pred_15_do_0.5.sh
