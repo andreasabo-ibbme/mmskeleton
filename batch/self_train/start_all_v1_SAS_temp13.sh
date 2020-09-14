8554cb51b427decc987c19d318891e8d8234b71e
Andrea Sabo <51453348+andreasabo-ibbme@users.noreply.github.com>
2020-09-14 09:35:01 -0400

    bash by temp size

---
+++ batch/self_train/start_all_v1_SAS_temp13.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# SAS
+cd /home/asabo/projects/def-btaati/asabo/mmskeleton/batch/self_train/SAS/v1/temp13
+sbatch SAS_0_120_pred_15_do_0.0.sh
+sbatch SAS_0_120_pred_15_do_0.1.sh
+sbatch SAS_0_120_pred_15_do_0.2.sh
+sbatch SAS_0_120_pred_15_do_0.3.sh
+sbatch SAS_0_120_pred_15_do_0.4.sh
+sbatch SAS_0_120_pred_15_do_0.5.sh
