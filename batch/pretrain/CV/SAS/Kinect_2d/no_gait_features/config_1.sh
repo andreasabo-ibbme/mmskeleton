51876b3f0314b4a36ffbba0bafa692fb6dfc460b
Andrea Sabo <51453348+andreasabo-ibbme@users.noreply.github.com>
2020-12-29 11:43:49 -0500

    kinect 2d setup

--- batch/pretrain/CV/SAS/Kinect_2d/no_gait_features/config_1.sh
+++ batch/pretrain/CV/SAS/Kinect_2d/no_gait_features/config_1.sh
@@ -18,4 +18,4 @@ module load cuda/10.1
 source ~/ENV/bin/activate
 
 cd /home/asabo/projects/def-btaati/asabo/mmskeleton
-python mmskl.py
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/CV_configs/SAS/kinect_2d/no_gait_features/train_cluster_2_120_pred_15_4_joints_do_0.2.yaml
---
+++ batch/pretrain/CV/start_SAS_kinect_2d.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /home/asabo/projects/def-btaati/asabo/mmskeleton/batch/pretrain/CV/SAS/Kinect_2d
+
+sbatch gait_features/config_1.sh
+sbatch gait_features/config_2.sh
+sbatch gait_features/config_3.sh
+sbatch gait_features/config_4.sh
+sbatch gait_features/config_5.sh
+
+sbatch no_gait_features/config_1.sh
+sbatch no_gait_features/config_2.sh
+sbatch no_gait_features/config_3.sh
+sbatch no_gait_features/config_4.sh
+sbatch no_gait_features/config_5.sh
