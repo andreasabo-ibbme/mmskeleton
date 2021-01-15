8ba0bf529672b89e59a0e11755b1387b6719e17a
Andrea Sabo <51453348+andreasabo-ibbme@users.noreply.github.com>
2021-01-15 08:40:42 -0500

    reduce time

--- batch/pretrain/SAS/v6/temp5/SAS_10_120_pred_15_do_0.2.sh
+++ batch/pretrain/SAS/v6/temp5/SAS_10_120_pred_15_do_0.2.sh
@@ -2,7 +2,7 @@
 #SBATCH --gres=gpu:1              # Number of GPUs (per node)
 #SBATCH --mem=16000               # memory (per node)
 #SBATCH --cpus-per-task=8
-#SBATCH --time=4-04:00            # time (DD-HH:MM)
+#SBATCH --time=1-04:00            # time (DD-HH:MM)
 #SBATCH --job-name=SAS_openpose_0.2
 #SBATCH --output=%x-%j_32hour.out
 #SBATCH --account=def-btaati
