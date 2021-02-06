1d7625cb476880ef966eaa2833bfa58c1a787380
Andrea Sabo <51453348+andreasabo-ibbme@users.noreply.github.com>
2021-02-06 18:59:51 -0500

    detectron batch files

---
+++ batch/pretrain/batch_start/start_UPDRS_2d_posetracker.sh
@@ -0,0 +1,73 @@
+cd /home/asabo/projects/def-btaati/asabo/mmskeleton/batch/pretrain/UPDRS/v6
+# SAS
+sbatch temp5/SAS_0_120_pred_15_do_0.0.sh
+sbatch temp5/SAS_2_120_pred_15_do_0.0.sh
+sbatch temp5/SAS_10_120_pred_15_do_0.0.sh
+
+sbatch temp9/SAS_0_120_pred_15_do_0.0.sh
+sbatch temp9/SAS_2_120_pred_15_do_0.0.sh
+sbatch temp9/SAS_10_120_pred_15_do_0.0.sh
+
+sbatch temp13/SAS_0_120_pred_15_do_0.0.sh
+sbatch temp13/SAS_2_120_pred_15_do_0.0.sh
+sbatch temp13/SAS_10_120_pred_15_do_0.0.sh
+
+sbatch temp5/SAS_0_120_pred_15_do_0.1.sh
+sbatch temp5/SAS_2_120_pred_15_do_0.1.sh
+sbatch temp5/SAS_10_120_pred_15_do_0.1.sh
+
+sbatch temp9/SAS_0_120_pred_15_do_0.1.sh
+sbatch temp9/SAS_2_120_pred_15_do_0.1.sh
+sbatch temp9/SAS_10_120_pred_15_do_0.1.sh
+
+sbatch temp13/SAS_0_120_pred_15_do_0.1.sh
+sbatch temp13/SAS_2_120_pred_15_do_0.1.sh
+sbatch temp13/SAS_10_120_pred_15_do_0.1.sh
+
+sbatch temp5/SAS_0_120_pred_15_do_0.2.sh
+sbatch temp5/SAS_2_120_pred_15_do_0.2.sh
+sbatch temp5/SAS_10_120_pred_15_do_0.2.sh
+
+sbatch temp9/SAS_0_120_pred_15_do_0.2.sh
+sbatch temp9/SAS_2_120_pred_15_do_0.2.sh
+sbatch temp9/SAS_10_120_pred_15_do_0.2.sh
+
+sbatch temp13/SAS_0_120_pred_15_do_0.2.sh
+sbatch temp13/SAS_2_120_pred_15_do_0.2.sh
+sbatch temp13/SAS_10_120_pred_15_do_0.2.sh
+
+sbatch temp5/SAS_0_120_pred_15_do_0.3.sh
+sbatch temp5/SAS_2_120_pred_15_do_0.3.sh
+sbatch temp5/SAS_10_120_pred_15_do_0.3.sh
+
+sbatch temp9/SAS_0_120_pred_15_do_0.3.sh
+sbatch temp9/SAS_2_120_pred_15_do_0.3.sh
+sbatch temp9/SAS_10_120_pred_15_do_0.3.sh
+
+sbatch temp13/SAS_0_120_pred_15_do_0.3.sh
+sbatch temp13/SAS_2_120_pred_15_do_0.3.sh
+sbatch temp13/SAS_10_120_pred_15_do_0.3.sh
+
+sbatch temp5/SAS_0_120_pred_15_do_0.4.sh
+sbatch temp5/SAS_2_120_pred_15_do_0.4.sh
+sbatch temp5/SAS_10_120_pred_15_do_0.4.sh
+
+sbatch temp9/SAS_0_120_pred_15_do_0.4.sh
+sbatch temp9/SAS_2_120_pred_15_do_0.4.sh
+sbatch temp9/SAS_10_120_pred_15_do_0.4.sh
+
+sbatch temp13/SAS_0_120_pred_15_do_0.4.sh
+sbatch temp13/SAS_2_120_pred_15_do_0.4.sh
+sbatch temp13/SAS_10_120_pred_15_do_0.4.sh
+
+sbatch temp5/SAS_0_120_pred_15_do_0.5.sh
+sbatch temp5/SAS_2_120_pred_15_do_0.5.sh
+sbatch temp5/SAS_10_120_pred_15_do_0.5.sh
+
+sbatch temp9/SAS_0_120_pred_15_do_0.5.sh
+sbatch temp9/SAS_2_120_pred_15_do_0.5.sh
+sbatch temp9/SAS_10_120_pred_15_do_0.5.sh
+
+sbatch temp13/SAS_0_120_pred_15_do_0.5.sh
+sbatch temp13/SAS_2_120_pred_15_do_0.5.sh
+sbatch temp13/SAS_10_120_pred_15_do_0.5.sh
