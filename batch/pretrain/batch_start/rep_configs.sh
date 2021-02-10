4f9342243d7f5a5534d29770dc1caa8e9dd748b8
Andrea Sabo <51453348+andreasabo-ibbme@users.noreply.github.com>
2021-02-10 10:46:47 -0500

    rep configs

---
+++ batch/pretrain/batch_start/rep_configs.sh
@@ -0,0 +1,8 @@
+cd /home/asabo/projects/def-btaati/asabo/mmskeleton/batch/pretrain/CV/retry
+
+# SAS
+sbatch SAS/config_1.sh
+sbatch SAS/config_2.sh
+sbatch SAS/config_3.sh
+
+sbatch UPDRS/config_1.sh
--- batch/pretrain/batch_start/rep_configs_alphapose.sh
+++
@@ -1,4 +0,0 @@
-cd /home/asabo/projects/def-btaati/asabo/mmskeleton/batch/pretrain/SAS/rep_configs_alphapose
-# SAS
-sbatch SAS_0_120_pred_15_do_0.1.sh
-sbatch SAS_0_120_pred_15_do_0.5.sh
