0aea34cda58ae5ac6227cb0fc3dab5b22c0c9019
Andrea Sabo <andrea.sabo@mail.utoronto.ca>
2021-02-18 12:09:39 -0500

    finish lower level batch files

---
+++ batch/pretrain/ablation_cnn/UPDRS/openpose/temp13/model_2/cnn_0.3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1              # Number of GPUs (per node)
+#SBATCH --mem=16000               # memory (per node)
+#SBATCH --cpus-per-task=8
+#SBATCH --time=0-20:00            # time (DD-HH:MM)
+#SBATCH --job-name=UPDRS_openpose_cnn
+#SBATCH --output=%x-%j_32hour.out
+#SBATCH --account=def-btaati
+
+#SBATCH --mail-user=andrea.sabo@mail.utoronto.ca
+#SBATCH --mail-type=FAIL
+
+
+module load python/3.6
+module load nixpkgs/16.09  gcc/8.3.0 
+module load cuda/10.1
+
+source ~/ENV/bin/activate
+
+cd /home/asabo/projects/def-btaati/asabo/mmskeleton
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/UPDRS/openpose/temp13/model_2/cnn_0.3.yaml
