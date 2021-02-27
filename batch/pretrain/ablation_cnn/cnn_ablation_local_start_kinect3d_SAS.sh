af315c2fda8d836b991adef52a8e2d9e3587817c
Andrea Sabo <andrea.sabo@mail.utoronto.ca>
2021-02-26 20:18:32 -0500

    local bash

---
+++ batch/pretrain/ablation_cnn/cnn_ablation_local_start_kinect3d_SAS.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+pyenv activate mmskeleton
+
+cd /home/saboa/code/mmskeleton
+
+# 
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp5/model_1/cnn_0.0.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp5/model_1/cnn_0.1.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp5/model_1/cnn_0.2.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp5/model_1/cnn_0.3.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp5/model_1/cnn_0.4.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp5/model_1/cnn_0.5.yaml
+
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp5/model_2/cnn_0.0.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp5/model_2/cnn_0.1.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp5/model_2/cnn_0.2.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp5/model_2/cnn_0.3.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp5/model_2/cnn_0.4.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp5/model_2/cnn_0.5.yaml
+
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp5/model_3/cnn_0.0.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp5/model_3/cnn_0.1.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp5/model_3/cnn_0.2.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp5/model_3/cnn_0.3.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp5/model_3/cnn_0.4.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp5/model_3/cnn_0.5.yaml
+
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp5/model_4/cnn_0.0.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp5/model_4/cnn_0.1.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp5/model_4/cnn_0.2.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp5/model_4/cnn_0.3.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp5/model_4/cnn_0.4.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp5/model_4/cnn_0.5.yaml
+
+
+
+#Temp 9
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp9/model_1/cnn_0.0.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp9/model_1/cnn_0.1.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp9/model_1/cnn_0.2.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp9/model_1/cnn_0.3.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp9/model_1/cnn_0.4.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp9/model_1/cnn_0.5.yaml
+
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp9/model_2/cnn_0.0.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp9/model_2/cnn_0.1.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp9/model_2/cnn_0.2.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp9/model_2/cnn_0.3.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp9/model_2/cnn_0.4.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp9/model_2/cnn_0.5.yaml
+
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp9/model_3/cnn_0.0.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp9/model_3/cnn_0.1.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp9/model_3/cnn_0.2.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp9/model_3/cnn_0.3.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp9/model_3/cnn_0.4.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp9/model_3/cnn_0.5.yaml
+
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp9/model_4/cnn_0.0.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp9/model_4/cnn_0.1.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp9/model_4/cnn_0.2.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp9/model_4/cnn_0.3.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp9/model_4/cnn_0.4.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp9/model_4/cnn_0.5.yaml
+
+
+
+#Temp 13
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp13/model_1/cnn_0.0.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp13/model_1/cnn_0.1.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp13/model_1/cnn_0.2.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp13/model_1/cnn_0.3.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp13/model_1/cnn_0.4.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp13/model_1/cnn_0.5.yaml
+
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp13/model_2/cnn_0.0.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp13/model_2/cnn_0.1.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp13/model_2/cnn_0.2.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp13/model_2/cnn_0.3.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp13/model_2/cnn_0.4.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp13/model_2/cnn_0.5.yaml
+
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp13/model_3/cnn_0.0.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp13/model_3/cnn_0.1.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp13/model_3/cnn_0.2.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp13/model_3/cnn_0.3.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp13/model_3/cnn_0.4.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp13/model_3/cnn_0.5.yaml
+
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp13/model_4/cnn_0.0.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp13/model_4/cnn_0.1.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp13/model_4/cnn_0.2.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp13/model_4/cnn_0.3.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp13/model_4/cnn_0.4.yaml
+python mmskl.py configs/recognition/tri/cluster/pred_pretrain/ablation_configs_cnn/SAS/kinect_3d/temp13/model_4/cnn_0.5.yaml
+
