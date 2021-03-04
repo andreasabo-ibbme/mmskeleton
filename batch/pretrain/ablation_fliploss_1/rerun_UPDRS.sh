b2f2960038cefa61152ea641f85b539bfa234b97
Andrea Sabo <andrea.sabo@mail.utoronto.ca>
2021-03-04 18:57:13 -0500

    set up flip ablation

--- batch/pretrain/ablation_fliploss_1/rerun_UPDRS.sh
+++ batch/pretrain/ablation_fliploss_1/rerun_UPDRS.sh
@@ -2,7 +2,7 @@
 
 cd /home/asabo/projects/def-btaati/asabo/mmskeleton/batch/pretrain/ablation_fliploss_1/UPDRS/alphapose
 
-sbatch gait_features/config_1.sh
+# sbatch gait_features/config_1.sh
 sbatch gait_features/config_2.sh
 sbatch gait_features/config_3.sh
 
@@ -26,7 +26,3 @@ cd /home/asabo/projects/def-btaati/asabo/mmskeleton/batch/pretrain/ablation_flip
 sbatch gait_features/config_1.sh
 sbatch gait_features/config_2.sh
 sbatch gait_features/config_3.sh
-
-sbatch no_gait_features/config_1.sh
-sbatch no_gait_features/config_2.sh
-sbatch no_gait_features/config_3.sh
