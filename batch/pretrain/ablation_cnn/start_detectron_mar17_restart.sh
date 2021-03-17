c428a946c0461b4106b1f9b1db07cec88b52505b
Andrea Sabo <51453348+andreasabo-ibbme@users.noreply.github.com>
2021-03-17 11:03:32 -0400

    march 17 cnn restart

---
+++ batch/pretrain/ablation_cnn/start_detectron_mar17_restart.sh
@@ -0,0 +1,84 @@
+# SAS
+cd /home/asabo/projects/def-btaati/asabo/mmskeleton/batch/pretrain/ablation_cnn/SAS
+
+sbatch detectron/temp5/model_2/cnn_0.4.sh
+sbatch detectron/temp5/model_4/cnn_0.0.sh
+sbatch detectron/temp5/model_4/cnn_0.1.sh
+
+#temp9
+sbatch detectron/temp9/model_1/cnn_0.5.sh
+
+sbatch detectron/temp9/model_4/cnn_0.0.sh
+sbatch detectron/temp9/model_4/cnn_0.1.sh
+sbatch detectron/temp9/model_4/cnn_0.2.sh
+sbatch detectron/temp9/model_4/cnn_0.3.sh
+
+
+#temp13
+sbatch detectron/temp13/model_1/cnn_0.1.sh
+sbatch detectron/temp13/model_1/cnn_0.2.sh
+sbatch detectron/temp13/model_1/cnn_0.4.sh
+
+sbatch detectron/temp13/model_3/cnn_0.1.sh
+sbatch detectron/temp13/model_3/cnn_0.5.sh
+
+sbatch detectron/temp13/model_4/cnn_0.1.sh
+sbatch detectron/temp13/model_4/cnn_0.2.sh
+sbatch detectron/temp13/model_4/cnn_0.3.sh
+sbatch detectron/temp13/model_4/cnn_0.4.sh
+sbatch detectron/temp13/model_4/cnn_0.5.sh
+
+#UPDRS
+cd /home/asabo/projects/def-btaati/asabo/mmskeleton/batch/pretrain/ablation_cnn/UPDRS
+
+sbatch detectron/temp5/model_1/cnn_0.2.sh
+
+sbatch detectron/temp5/model_2/cnn_0.1.sh
+sbatch detectron/temp5/model_2/cnn_0.3.sh
+sbatch detectron/temp5/model_2/cnn_0.4.sh
+sbatch detectron/temp5/model_2/cnn_0.5.sh
+
+sbatch detectron/temp5/model_3/cnn_0.0.sh
+sbatch detectron/temp5/model_3/cnn_0.2.sh
+
+sbatch detectron/temp5/model_4/cnn_0.0.sh
+sbatch detectron/temp5/model_4/cnn_0.1.sh
+sbatch detectron/temp5/model_4/cnn_0.2.sh
+sbatch detectron/temp5/model_4/cnn_0.3.sh
+
+
+#temp9
+sbatch detectron/temp9/model_1/cnn_0.0.sh
+sbatch detectron/temp9/model_1/cnn_0.1.sh
+sbatch detectron/temp9/model_1/cnn_0.2.sh
+sbatch detectron/temp9/model_1/cnn_0.3.sh
+sbatch detectron/temp9/model_1/cnn_0.4.sh
+sbatch detectron/temp9/model_1/cnn_0.5.sh
+
+
+sbatch detectron/temp9/model_2/cnn_0.2.sh
+sbatch detectron/temp9/model_2/cnn_0.4.sh
+
+sbatch detectron/temp9/model_4/cnn_0.0.sh
+sbatch detectron/temp9/model_4/cnn_0.1.sh
+sbatch detectron/temp9/model_4/cnn_0.2.sh
+sbatch detectron/temp9/model_4/cnn_0.4.sh
+
+#temp13
+sbatch detectron/temp13/model_1/cnn_0.0.sh
+sbatch detectron/temp13/model_1/cnn_0.1.sh
+sbatch detectron/temp13/model_1/cnn_0.2.sh
+sbatch detectron/temp13/model_1/cnn_0.3.sh
+
+sbatch detectron/temp13/model_2/cnn_0.1.sh
+sbatch detectron/temp13/model_2/cnn_0.5.sh
+
+sbatch detectron/temp13/model_3/cnn_0.1.sh
+sbatch detectron/temp13/model_3/cnn_0.2.sh
+sbatch detectron/temp13/model_3/cnn_0.4.sh
+sbatch detectron/temp13/model_3/cnn_0.5.sh
+
+sbatch detectron/temp13/model_4/cnn_0.0.sh
+sbatch detectron/temp13/model_4/cnn_0.1.sh
+sbatch detectron/temp13/model_4/cnn_0.3.sh
+sbatch detectron/temp13/model_4/cnn_0.4.sh
